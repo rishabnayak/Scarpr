1c7f7784befb405efeb5e08bd0e125b67a31b21c
Rishab Nayak <rishab@bu.edu>
2020-06-16 17:48:02 -0400

    ??

--- batteryservice/BatteryService.py
+++ batteryservice/BatteryService.py
@@ -4,7 +4,7 @@ from BatteryLevelCharacteristic import *
 class BatteryService(BlenoPrimaryService):
     def __init__(self):
         BlenoPrimaryService.__init__(self, {
-          'uuid': '0001',
+          'uuid': '18A',
           'characteristics': [
               BatteryLevelCharacteristic()
           ]})
